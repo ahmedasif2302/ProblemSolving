f0439c717f55431d8a6dd6bc470ab25def298e57
ahmedasif <ahmedasif2302@gmail.com>
2025-07-21 13:50:15 +0530

    reverse an array

--- arrays/344.js
+++ arrays/344.js
@@ -3,13 +3,14 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function (s) {
-  let c = s.length - 1;
-  for (let i = 0; i < s.length; i++) {
-    s[i] = s[c];
-    c--;
+ let length = s.length -1
+  let halfLen = Math.floor(s.length/2)
+  for(let i =0; i < halfLen;i++){
+    let temp = s[i];
+    s[i] = s[length -i];
+    s[length -i] = temp
   }
-
-  return s;
+    return s
 };
 
 console.log(reverseString(["h", "e", "l", "l", "o"]));
